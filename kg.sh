8bf6c82678d437ed98217f2462b66db871c359b0
Cloyster Veeta <cveeta@un-i4.kzn.21-school.ru>
2021-02-27 15:11:27 +0300

    nginx done no ssh

--- kg.sh
+++ kg.sh
@@ -1,5 +1,9 @@
-kubectl delete pods nginx
-kubectl get pods
+
+kubectl delete deploy nginx-deployment
+kubectl delete svc nginx-svc
+#kubectl delete pods nginx
+#kubectl get pods
 docker build -t nginx_img .
+kubectl apply -f configmap.yaml
 kubectl apply -f ng.yaml
-kubectl get pods
+kubectl get deploy
--- serv.sh
+++ serv.sh
@@ -1,10 +1,23 @@
-brew reinstall minikube
+#rm .minikube
+#rm .docker
 
-mv .docker ./goinfre
-ln -s ./goinfre/.docker .docker
+brew reinstall minikube
+#
+#mv .docker ./goinfre
+#ln -s ./goinfre/.docker .docker
 
 minikube start --vm-driver=virtualbox
-mv .minikube ./goinfre
-ln -s ./goinfre/.minikube .minikube
+#mv .minikube ./goinfre
+#ln -s ./goinfre/.minikube .minikube
 
 eval $(minikube docker-env)
+
+minikube addons enable metallb
+kubectl delete deploy nginx-deployment
+kubectl delete svc nginx-svc
+#kubectl delete pods nginx
+#kubectl get pods
+docker build -t nginx_img .
+kubectl apply -f configmap.yaml
+kubectl apply -f ng.yaml
+kubectl get deploy
